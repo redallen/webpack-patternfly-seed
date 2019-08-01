13be93caacaea80a21b23a7647ce36c1599bd413
redallen <zallen@redhat.com>
2019-08-01 11:52:13 -0400

    Simple icon

---
+++ src/app.js
@@ -0,0 +1,8 @@
+import React from 'react';
+import { Button } from 'patternfly-react';
+// import '@patternfly/react-core/dist/styles/base.css';
+import 'patternfly-react/dist/css/patternfly-react.css';
+
+export default () => (
+  <Button>A PF3 Button</Button>
+);
